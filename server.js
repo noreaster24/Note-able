5e50aeddc440f1aa48b2465f028eb45657d37be1
Joe Porrazzo <joepa2624@me.com>
2021-05-30 14:06:25 -0700

    html linked and functioning

--- server.js
+++ server.js
@@ -5,18 +5,54 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 const { notes } = require('./data/notes.json')
 
-app.get('/', (req, res) => {
-    res.send('hello, world!');
-});
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './data/notes.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+};
+
+function validateNote(note) {
+    if (!note.title || typeof note.title !== 'string') {
+        return false;
+    }
+    if (!note.text || typeof note.text !== 'string') {
+        return false;
+    }
+    return true;
+};
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
+app.post('/api/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
 
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted!');
+    } else {
+    const note = createNewNote(req.body, notes);
+
+    res.json(note);
+    }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+}) ;
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
 
 app.listen(PORT, () => {
-    console.log(`API server now on port`);
+    console.log(`API server now on port ${PORT}!`);
 });
